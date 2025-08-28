dbf8957e3c57af87c0ca8a658108ae68af419f2d
ponta <m8wjrzja.a-wat@nifty.com>
2025-08-28 20:21:24 +0900

    わからんねぇ

--- ponpon/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-    console.log('hello!!');
-    context.res = {
-        status: 200,
-        body: "こんにちはぽんた!Azure Functionsが元気に動いてるよ"
-    };
-};
